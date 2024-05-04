54f214908016509ab023126e7f94ea7c9b7a514e
MoosaTae <sadhis.tae@gmail.com>
2024-05-04 13:35:16 +0700

    override and lowercase

--- src/entity/rocket/Bomb.java
+++ src/entity/rocket/Bomb.java
@@ -7,16 +7,17 @@ import static application.Main.HEIGHT;
 import static application.Main.score;
 
 public class Bomb extends Rocket implements Updatable {
-    private int SPEED = (score / 15) + 2;
+    private int speed = (score / 15) + 2;
     private int health = 5;
 
     public Bomb(int posX, int posY, int size, Image image) {
         super(posX, posY, size, image);
     }
 
+    @Override
     public void update() {
         super.update();
-        if(!isExploding() && !isDestroyed()) setPosY(getPosY() + this.SPEED);
+        if(!isExploding() && !isDestroyed()) setPosY(getPosY() + this.speed);
         if(getPosY() > HEIGHT) setDestroyed(true);
     }
 
